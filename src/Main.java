ecbe86d2cbefe1e1d05d073a6aa596cfb64c400d
KovalskaOlha <unlimolg@gmail.com>
2023-03-28 20:38:08 -0400

    I have successfuly resolved the issue and code is ready to be merged in the sun branch

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ public class Main {
 
         featureSun Xron=new featureSun();
         Xron.temperature="123123456k";
-        Xron.radius=1312323233;
+        Xron.radius="1312323233";
         Xron.color="pink";
     }
 }
